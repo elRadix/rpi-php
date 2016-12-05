086283ed89b21cdc1d2a6f909560e6e18084ac00
Tobias Hargesheimer <tobiash87@gmail.com>
2016-12-05 19:30:22 +0100

    update

--- php5-fpm/entrypoint.sh
+++ php5-fpm/entrypoint.sh
@@ -9,6 +9,12 @@ fi
 sed -i -e 's/display_errors = Off/display_errors = On/g' /etc/php5/fpm/php.ini
 sed -i -e 's/upload_max_filesize = 2M/upload_max_filesize = 250M/g' /etc/php5/fpm/php.ini
 
+sed -i -e 's/error_log = \/var\/log\/php5-fpm.log/error_log = \/proc\/self\/fd\/2/g' /etc/php5/fpm/php-fpm.conf
+sed -i -e 's/;daemonize = yes/daemonize = no/g' /etc/php5/fpm/php-fpm.conf
+
+sed -i -e 's/;access.log = log\/$pool.access.log/access.log = \/proc\/self\/fd\/2/g' /etc/php5/fpm/pool.d/www.conf
+sed -i -e 's/;catch_workers_output = yes/catch_workers_output = yes/g' /etc/php5/fpm/pool.d/www.conf
+sed -i -e 's/;clear_env = no/clear_env = no/g' /etc/php5/fpm/pool.d/www.conf
 sed -i -e 's/listen = \/var\/run\/php5-fpm.sock/listen = [::]:9000/g' /etc/php5/fpm/pool.d/www.conf
 
 # exec CMD
